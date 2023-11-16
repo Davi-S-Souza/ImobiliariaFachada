15282db4c0e1d9c97dc4a6ad8de97af524910b22
Kewit <137229111+Kewit@users.noreply.github.com>
2023-11-16 19:19:08 -0300

    Deletando erro

--- ImobiliariaFachada-main/src/main/java/com/example/demo/repository/FuncionariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Funcionarios;
-
-public interface FuncionariosRepository extends JpaRepository<Funcionarios,String>{
-    
-}
--- ImobiliariaFachada-main/src/main/java/com/example/demo/repository/ProprietarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Proprietario;
-
-public interface ProprietarioRepository extends JpaRepository<Proprietario, Long>{
-    
-}
